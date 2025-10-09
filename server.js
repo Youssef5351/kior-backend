9b3d74cf3693bad26f74589df43ebbc560a7452c
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-10-10 01:51:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const PORT = 5000;
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:'https://kior.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
